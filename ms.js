3264f36a5da69f65b45b845d66f436c14a51c726
baigsameer <jamalrana789@gmail.com>
2022-10-29 21:09:02 +0500

    table commit

---
+++ ms.js
@@ -0,0 +1,76 @@
+let value = prompt("enter vale")
+
+var text = document.createElement("h2")
+text.innerHTML = value * 1;
+document.body.appendChild(text)
+
+var text = document.createElement("h2")
+text.innerHTML = value * 2;
+document.body.appendChild(text)
+
+var text = document.createElement("h2")
+text.innerHTML = value * 3;
+document.body.appendChild(text)
+
+var text = document.createElement("h2")
+text.innerHTML = value * 4;
+document.body.appendChild(text)
+
+var text = document.createElement("h2")
+text.innerHTML = value * 5;
+document.body.appendChild(text)
+
+var text = document.createElement("h2")
+text.innerHTML = value * 6;
+document.body.appendChild(text)
+
+var text = document.createElement("h2")
+text.innerHTML = value * 7;
+document.body.appendChild(text)
+
+var text = document.createElement("h2")
+text.innerHTML = value * 8;
+document.body.appendChild(text)
+
+var text = document.createElement("h2")
+text.innerHTML = value * 9;
+document.body.appendChild(text)
+
+var text = document.createElement("h2")
+text.innerHTML = value * 10;
+document.body.appendChild(text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
